28027e73df2be44d4be07f43f44b0127d2656b9c
ALIRAZA47 <raza2018@namal.edu.pk>
2019-11-16 17:07:27 +0500

    Cleaned up CPP file to start work

--- projectC++/projectC++.cpp
+++ projectC++/projectC++.cpp
@@ -7,52 +7,17 @@ using namespace std;
 int newVar;
 
 const int NumberOfTotalAccounts = 5;
-int check;
 
-int uzair;
-<<<<<<< HEAD
-<<<<<<< HEAD
-int uzair2;
-=======
-int ali2;
-<<<<<<< HEAD
-int ali3;
-=======
->>>>>>> a6f27951201a7fd95164368f78cb9f399264dbdf
->>>>>>> 847e8c2ac9ad71effb9272d50ed6894073d6ce81
-=======
->>>>>>> 39a27b15b0a2cc2a3f21ef93deb9ba104f8b545f
 class uzair
 {
 private:
-<<<<<<< HEAD
-	int a;
-public:
-	int getdata()
-	{
-		return a + a;
-	}
 
-
-=======
-	int no;
-public:
-	int getNo()
-	{
-		int a;
-		a = 12;
-		return no+a;
-	}
->>>>>>> new
 };
 
 int main()
 {
-	std::cout << "Hello World!\n";
-	cout << "hello again\n";
-	cout << "hello\n";
-	cout << "Halo Munda" << endl;
-
+	
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
